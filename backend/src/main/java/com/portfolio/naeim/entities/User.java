780aadeec348e8a9603045813764b9f0c0451c2c
Naeim <naeimr4@gmail.com>
2024-09-17 12:21:15 +0000

    used lombok annotations for user entity

--- backend/src/main/java/com/portfolio/naeim/entities/User.java
+++ backend/src/main/java/com/portfolio/naeim/entities/User.java
@@ -3,11 +3,15 @@ package com.portfolio.naeim.entities;
 import com.portfolio.naeim.entities.base.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
