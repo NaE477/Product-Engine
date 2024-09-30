e64d93d737f4afb4f59981caeb75b1d1c50113c9
Naeim <naeimr4@gmail.com>
2024-09-30 08:18:04 +0000

    basic controller for user

--- backend/src/main/java/com/portfolio/naeim/controller/UserController.java
+++ backend/src/main/java/com/portfolio/naeim/controller/UserController.java
@@ -1,17 +1,44 @@
 package com.portfolio.naeim.controller;
 
+import com.portfolio.naeim.dto.UserDTO;
 import com.portfolio.naeim.entities.User;
+import com.portfolio.naeim.service.interfaces.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.portfolio.naeim.dto.DtoMapper.toUserDTO;
+import static com.portfolio.naeim.dto.DtoMapper.toUserEntity;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
-    @GetMapping("/api/user")
-    public ResponseEntity<User> getUser() {
-        return new ResponseEntity<>(new User("john_doe", "john@example.com"), HttpStatus.OK);
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/api/user")
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
+        User user = userService.createUser(toUserEntity(userDTO));
+        return new ResponseEntity<>(toUserDTO(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/users")
+    public Page<User> getUsers(Pageable pageable) {
+        return userService.getUsersByPage(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        UserDTO userDTO = toUserDTO(user);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
 }
