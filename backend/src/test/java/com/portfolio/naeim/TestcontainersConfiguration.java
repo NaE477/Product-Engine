4aafcb18b9ca322ef7442b94e9e735c986d72c95
Naeim <naeimr4@gmail.com>
2024-10-04 21:03:43 +0000

    added better testing solution for TestcontainersConfiguration

--- backend/src/test/java/com/portfolio/naeim/TestcontainersConfiguration.java
+++ backend/src/test/java/com/portfolio/naeim/TestcontainersConfiguration.java
@@ -1,5 +1,7 @@
 package com.portfolio.naeim;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +11,28 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
+    private PostgreSQLContainer<?> postgresqlContainer;
+
     @Bean
     @ServiceConnection
-    PostgreSQLContainer<?> postgresqlContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.2"));
+    public PostgreSQLContainer<?> postgresqlContainer() {
+        if (postgresqlContainer == null) {
+            postgresqlContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.2"));
+        }
+        return postgresqlContainer;
     }
 
+    @BeforeEach
+    void setUp() {
+        if (!postgresqlContainer.isRunning()) {
+            postgresqlContainer.start();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (postgresqlContainer.isRunning()) {
+            postgresqlContainer.stop();
+        }
+    }
 }
